1d9e77435c415868a0b232184a73433e46369a62
scv718 <63276430+scv718@users.noreply.github.com>
2020-07-02 17:32:38 +0900

    Update newtest.py

--- newtest.py
+++ newtest.py
@@ -7,6 +7,22 @@ embed=discord.Embed(title="   ", description="  ", color=0x00aaaa)
 @client.event
 
 
+import discord
+import random
+import os
+
+
+client = discord.Client()
+embed=discord.Embed(title="   ", description="   ", color=0x00aaaa)
+count = [1, 2]
+
+
+@client.event
+
+
+
+
+
 async def on_message(message):
     if message.content.startswith("!선원"):
         await message.channel.send("```렙업당 각 레벨별 최대 수치 - 순수한 선원 \n"
@@ -158,18 +174,9 @@ async def on_message(message):
         embed.set_image(url="https://media.discordapp.net/attachments/727925313497071698/728136520963457044/5a1d66e3af037f92.png")
         await message.channel.send(embed=embed)
 
-    if message.content.startswith("소라고둥님"):
+    if message.content.startswith("소라고둥님") or message.content.startswith(tuple("마법의 소라고둥님".split())):
         number = random.randrange(11)
-        if number % 2 == 1:
-            embed.set_image(url="https://cdn.discordapp.com/attachments/727925313497071698/728147135593316372/yes.gif")
-            await message.channel.send(embed=embed)
-        else:
-            embed.set_image(url="https://cdn.discordapp.com/attachments/727925313497071698/728147124621017158/no.jpg")
-            await message.channel.send(embed=embed)
-
-    if message.content.startswith("마법의소라고둥님"):
-        number = random.randrange(11)
-        if number % 2 == 1:
+        if random.choice(count) == 1:
             embed.set_image(url="https://cdn.discordapp.com/attachments/727925313497071698/728147135593316372/yes.gif")
             await message.channel.send(embed=embed)
         else:

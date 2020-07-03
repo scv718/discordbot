72be01c66f9e0a7e705bbab753c891314219d073
scv718 <63276430+scv718@users.noreply.github.com>
2020-07-03 15:39:50 +0900

    Update newtest.py

--- newtest.py
+++ newtest.py
@@ -183,17 +183,17 @@ async def on_message(message):
             
             
     if message.content.startswith("!엘렌데"):
-        await message.channel.send(":heart: :heart: :heart: :heart: :heart: :heart:\n"
-                                   "엘렌데넌아이돌이야\n"
-                                   "렌                               이\n"
-                                   "데                               돌\n"
-                                   "넌                               이\n"
-                                   "아                               아\n"
-                                   "이                               넌\n"
-                                   "돌                               데\n"
-                                   "이                               렌\n"
-                                   "야이돌이아넌데렌엘\n"
-                                   ":heart: :heart: :heart: :heart: :heart: :heart:")
+        await message.channel.send(":heart::heart::heart::heart::heart::heart::heart::heart::heart:\n"
+      ":heart:엘렌데넌아이돌이야:heart:\n"
+      ":heart:렌               　           이:heart:\n"
+      ":heart:데             엘             돌:heart:\n"
+      ":heart:넌             렌             이:heart:\n"
+      ":heart:아             데             아:heart:\n"
+      ":heart:이             귓             넌:heart:\n"
+      ":heart:돌            :heart_eyes:    　    데:heart:\n"
+      ":heart:이                      　    렌:heart:\n"
+      ":heart:야이돌이아넌데렌엘:heart:\n"
+      ":heart::heart::heart::heart::heart::heart::heart::heart::heart:\n")
         
         
 access_token = os.environ["BOT_TOKEN"]
